4f2629538a1cf2da1b34fbe64d3e026b2a656b8c
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:32:54 +0700

    Tugas 20 JS
    Buatlah pencarian kata “are”,”words” dalam kalimat :
    
     “There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.”

---
+++ tugas20.js
@@ -0,0 +1,7 @@
+function carikata(){
+  var string = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+
+  console.log(string.match("are"))
+  console.log(string.match(/words/))
+}
+carikata()
